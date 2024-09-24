0ec97e61d94d760bc8bb43a4b96eae4cbcdf10f0
Aroa-web <aroa.rivas.2003@gmail.com>
2024-09-24 14:29:44 +0200

    first commit

---
+++ prueba-en-clase/main.js
@@ -0,0 +1,9 @@
+let number = 6;
+console.log(number);
+
+number =[1,4,6,"abc",false];
+console.log(number);
+
+let persona ={};
+console.log(persona);
+//console.log(trypeof persona);
---
+++ prueba-en-clase/main2.js
@@ -0,0 +1,15 @@
+let person ="nuria";
+let city = "málaga";
+let str = `hi${person}, are you in ${city}`;
+console.log(str);
+
+
+let str2 = `hi${person}, 
+are you in:
+ ${city}`;
+ console.log(str2);
+
+ let str3 = 'hi'+person + 
+ 'are you in:' +
+ city;
+console.log(str3);
