92c1680aae1ec8db6b5fee956adb9c2be92ee251
aroa <aroa.rivas.2003@gmail.com>
2024-10-07 20:45:01 +0200

    nuevo ejercicio

---
+++ json/Libros.js
@@ -0,0 +1,57 @@
+
+   const books= [
+      {
+        "title": "Cien años de soledad",
+        "genre": "Realismo mágico",
+        "authors": ["Gabriel García Márquez"],
+        "pages": 417,
+        "published": "1967-06-05",
+        "read": true,
+        "website": ""
+      },
+      {
+        "title": "Don Quijote de la Mancha",
+        "genre": "Novela",
+        "authors": ["Miguel de Cervantes"],
+        "pages": 1023,
+        "published": "1605-01-16",
+        "read": true,
+        "website": ""
+      },
+      {
+        "title": "La sombra del viento",
+        "genre": "Novela histórica",
+        "authors": ["Carlos Ruiz Zafón"],
+        "pages": 487,
+        "published": "2001-04-17",
+        "read": false,
+        "website": ""
+      },
+      {
+        "title": "El amor en los tiempos del cólera",
+        "genre": "Novela",
+        "authors": ["Gabriel García Márquez"],
+        "pages": 348,
+        "published": "1985-03-05",
+        "read": true,
+        "website": ""
+      },
+      {
+        "title": "Pedro Páramo",
+        "genre": "Realismo mágico",
+        "authors": ["Juan Rulfo"],
+        "pages": 124,
+        "published": "1955-07-01",
+        "read": false,
+        "website": ""
+      },
+      {
+        "title": "Los detectives salvajes",
+        "genre": "Novela",
+        "authors": ["Roberto Bolaño"],
+        "pages": 388,
+        "published": "1998-01-01",
+        "read": true,
+        "website": ""
+      }
+    ]
