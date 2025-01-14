7be71c70db1acb73ce7d13b124ab35e445439df4
Aroa <aroa.rivas.2003@gmail.com>
2025-01-14 19:49:39 +0100

    repaso

---
+++ Repaso/script.js
@@ -0,0 +1,10 @@
+document.querySelector("#generate-table"),addEventListener("submit,(e")=>{
+    e.preventDefault();
+    let isValid = true;
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    document.querySelector("#name-error")
+}
