f63f8a39498f643260c98d2d4f169a7b02535b84
Aroa <aroa.rivas.2003@gmail.com>
2025-01-31 19:32:45 +0100

    trabajo terminado

--- SegundoTrimester/Angular/home-app/src/app/app.component.ts
+++ SegundoTrimester/Angular/home-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
+  standalone:true,
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
