c4ae43b8b4aff0a23026b7ec3b250503af8f81c4
Aroa <aroa.rivas.2003@gmail.com>
2025-02-04 18:46:05 +0100

    cambie el total de eventos

--- SegundoTrimester/Angular/mi-app/src/app/components/event-list/event-list.component.ts
+++ SegundoTrimester/Angular/mi-app/src/app/components/event-list/event-list.component.ts
@@ -1,34 +1,38 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Asegúrate de importar CommonModule
+import { CommonModule } from '@angular/common';  
 import { LoggerService } from '../../services/logger.service';
 import { Event } from '../../models/event.model';
 
 @Component({
   selector: 'app-event-list',
-  standalone: true,  // Marca este componente como standalone
-  imports: [CommonModule],  // Asegúrate de agregar CommonModule aquí
+  standalone: true,  
+  imports: [CommonModule],  
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent {
+
+  allEvents: Event[] =[];
   events: Event[] = [];
   eventsCount = { log: 0, warm: 0, error: 0 };
 
   constructor(private loggerService: LoggerService) {}
 
   ngOnInit(): void {
-    this.events = this.loggerService.getEvents();
+    this.allEvents= this.loggerService.getEvents();
     this.updateEventCount();
+    this.events = [...this.allEvents];
   }
 
   updateEventCount(): void {
-    this.eventsCount.log = this.events.filter(event => event.category === 'log').length;
-    this.eventsCount.warm = this.events.filter(event => event.category === 'warm').length;
-    this.eventsCount.error = this.events.filter(event => event.category === 'error').length;
+    this.eventsCount={
+      log: this.allEvents.filter(event => event.category === 'log').length,
+      warm: this.allEvents.filter(event => event.category === 'warm').length,
+      error: this.allEvents.filter(event => event.category === 'error').length
+    }
   }
 
   onFilterCategory(category: string): void {
-    this.events = this.loggerService.getEvents().filter(event => event.category === category);
-    this.updateEventCount();
+   this.events = this.allEvents.filter(event => event.category === category);
   }
 }
