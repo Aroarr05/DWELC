96beeb1e38ba8b493c382b108ab72c2564630536
Aroa-web <aroa.rivas.2003@gmail.com>
2025-03-21 09:21:54 +0100

    recupercion

---
+++ Recuperacion/Ejercicios/ejercicioAlumnos/alumnos.js
@@ -0,0 +1,10 @@
+const alumnos =[
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"},
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"},
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"},
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"},
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"},
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"},
+    {nombre: "Aroa", apellido: "Rivas", fechaNacimiento: "05/05/2003", curso: "2daw"}
+
+]
