a06663ef1d50c7510aba66c9a2e563a7caf586d3
Aroa <aroa.rivas.2003@gmail.com>
2025-03-24 11:41:59 +0100

    select se ven

--- Recuperacion/Ejercicios/ejercicioAlumnos/alumnos.js
+++ Recuperacion/Ejercicios/ejercicioAlumnos/alumnos.js
@@ -2,41 +2,60 @@ document.addEventListener("DOMContentLoaded", ()=>{
     fetch("../../assets/json/alumnos.json")
         .then(response => response.json())
         .then(data => {
-            cargarOpciones();
+            cargarSelct();
+            selectFecha();
+            selectOrdenar();
             manejarFiltros(data);
-            mostrarDatos(data)
+            //mostrarDatos(data);
         })
         .catch(error => console.error ("Error al cargar el Json", error));    
 })
 
-let alumnosDatos = [];
+//let alumnosDatos = [];
 
-function cargarOpciones(){
+function cargarSelct(){
     const opciones = [ "Comenzar por", "Terminar en", "Contiene", "Igual a", "Está vacío", "Está relleno"];
     const selects = document.querySelectorAll("select");
 
     selects.forEach(select =>{
         opciones.forEach(opcion => {
-            let opt = document.createElement ("option");
-            opt.value = opcion.toLowerCase().replace(" ", "_");
-            opt.textContent=opcion;
-            select.appendChild(opt);
+            let opc = document.createElement ("option");
+            opc.value = opcion.toLowerCase().replace(" ", "_");
+            opc.textContent=opcion;
+            select.appendChild(opc);
         })
     })
 }
 
-function manejarFiltros(){
-    document.querySelectorAll("select"). forEach(select =>{
-        select.addEventListener("change",(event) =>{
-            const input1 = event.target.nextElementSibling.nextElementSibling;
-            const input2 = input1.nextElementSibling.nextElementSibling;
-            if (event.target.value === "esta_vacio"|| event.target.value==="esta_relleno"){
-                input1.disabled = true;
-                input2.disabled = true;
-            }else{
-                input1.disabled = false;
-                input2.disabled = false;
-            }
-        })
+function selectFecha(){
+    const opciones = ["Igual a", "Comprenmdido entre", "Posterior a", "Aterior a","Está vacío","Está relleno"];
+    const fechaSelect = document.querySelector("#fechaNacimiento-select");
+
+    fechaSelect.innerHTML = ' ';
+
+    opciones.forEach(option =>{
+        const opc = document.createElement("option");
+        opc.value = option.toLowerCase().replace(" ", "_");
+        opc.textContent = option;
+        fechaSelect.appendChild(opc);
     })
+
+}
+
+function selectOrdenar(){
+    const opciones = ["Asccendente","Descendente","Nombre","Apellido","Curso","Fecha de Nacimiento"];
+    const ordenarSelect = document.querySelector("#ordenar-select");
+
+    ordenarSelect.innerHTML = ' ';
+
+    opciones.forEach(option => {
+        const opc = document.createElement("option");
+        opc.value = option.toLowerCase().replace(" ", "_");
+        opc.textContent = option;
+        ordenarSelect.appendChild(opc);
+    })
+}
+
+function manejarFiltros(){
+  
 }
