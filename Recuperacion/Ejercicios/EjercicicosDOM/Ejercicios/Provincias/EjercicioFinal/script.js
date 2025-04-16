f82472baacb23fac105de438d8eda021da55f645
Aroa <aroa.rivas.2003@gmail.com>
2025-04-16 16:29:52 +0200

    fetch funcinando

--- Recuperacion/Ejercicios/EjercicicosDOM/Ejercicios/Provincias/EjercicioFinal/script.js
+++ Recuperacion/Ejercicios/EjercicicosDOM/Ejercicios/Provincias/EjercicioFinal/script.js
@@ -1,23 +1,45 @@
-let listaDatos = [];
+let datosCsv = [];
+let datosJson = [];
 
+document.addEventListener("DOMContentLoaded", () => {
+    cargarDatosCsv("../../../../../assets/json/da_centros.csv");
+    cargarDatosJson("../../../../../assets/json/provincias.json");
+});
 
-document.addEventListener("DOMContentLoaded", ()=>{
-
-
-    
-    cargarDatos("../../assets/json/provincias.json");
-    console.log(listaDatos);
-})
-
-function cargarDatos (url){
+function cargarDatosCsv(url) {
     fetch(url)
-    .then(response =>response.json())
-    .then(data =>{
-        listaDatos =  data.provincias;
-    })
-    .catch(error => console.error("Error al cargar datos:", error))
+        .then(response => response.text())
+        .then(data => {
+            datosCsv = procesarCSV(data);
+        })
+        .catch(error => console.error("Error al cargar el CSV:", error));
 }
 
-// recorro todo el froreal y solo selecionelo lo que quiero 
-// centro 
-// hacer un mapa
+function cargarDatosJson(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            datosJson = data.provincias;
+            console.log("Contenido del JSON:", datosJson);
+        })
+        .catch(error => console.error("Error al cargar el JSON:", error));
+}
+
+function procesarCSV(data) {
+    const filas = data.split('\n');
+    const listaPeque = [];
+
+    for (let i = 1; i < filas.length; i++) {
+        const columna = filas[i].split(';');
+        if (columna.length >= 8) {  
+            const MinCSV = {
+                D_DENOMINA: columna[2],
+                D_ESPECIFICA: columna[3],
+                D_MUNICIPIO: columna[8]
+            };
+            listaPeque.push(MinCSV);
+        }
+    }
+    console.log("Contenido procesado del CSV:", listaPeque); 
+    return listaPeque;
+}
