533d62f86fed23183df2bd8d6efebfa2fc1810fd
Aroa-web <aroa.rivas.2003@gmail.com>
2025-04-25 11:14:41 +0200

    guardar cambios

--- Recuperacion/Ejercicios/EjercicicosDOM/Animales/script.js
+++ Recuperacion/Ejercicios/EjercicicosDOM/Animales/script.js
@@ -1,23 +1,56 @@
+let animalSeleccionado = null;
+const distintasRazas = {
+    gato: ["Siamens", "Persa", "Ragdoll"],
+    perro: ["Labrador", "Bulldog", "Husky"]
+};
 
-document.addEventListener("DOMContentLoaded",()=>{
-    mostrarDatos(animals);
-})
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarDatos(animals); 
+});
 
-//MOSTRAR LOS DATOS 
-function mostrarDatos(data){
+function mostrarDatos(data) {
     const tbody = document.querySelector("#animales-list tbody");
-    tbody.innerHTML = "";
+    tbody.innerHTML = ""; 
 
-    data.forEach(item => {
+    data.forEach((item, index) => {
         const fila = document.createElement("tr");
         fila.innerHTML = `
-        <td>${item.name}</td>
-        <td>${item.species}</td>
-        <td>${item.foods.likes}</td>
-        <td>${item.foods.dislikes}</td>
-        <td><button id="editar">Editar</button><td>
+            <td>${item.name}</td>
+            <td>${item.species}</td>
+            <td>${item.foods.likes}</td>
+            <td>${item.foods.dislikes}</td>
+            <td><button class="editar" data-index="${index}">Editar</button></td>
         `;
         tbody.appendChild(fila);
     });
 
+    /*tbody.addEventListener("click", (e) => {
+        if (e.target.classList.contains("editar")) {
+            const index = e.target.getAttribute("data-index");
+            cargarFormulario(animals[index]);
+        }
+        
+        if (e.target.closest('tr') && !e.target.classList.contains('editar')) {
+            const filaActual = e.target.closest('tr');
+            destacarFila(filaActual);
+        }
+    });*/
 }
+
+/*function cargarFormulario(animals) {
+    document.querySelector("#nombre").value = animals.name;
+    document.querySelector("#especie").value = animals.species;
+    document.querySelector("#likes").value = animals.foods.likes;
+    document.querySelector("#dislikes").value = animals.foods.dislikes;
+}*/
+
+/*let filaAnteriorSeleccionada = null;
+
+function destacarFila(filaActual) {
+    if (filaAnteriorSeleccionada) {
+        filaAnteriorSeleccionada.classList.remove("fila-seleccionada");
+    }
+    
+    filaActual.classList.add("fila-seleccionada");
+    filaAnteriorSeleccionada = filaActual;
+}*/
