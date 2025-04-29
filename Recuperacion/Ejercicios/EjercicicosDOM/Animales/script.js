677c894e09daed31cd8fda2c6e63ab52d357b3d2
Aroa <aroa.rivas.2003@gmail.com>
2025-04-29 11:49:18 +0200

    no esta terminado

--- Recuperacion/Ejercicios/EjercicicosDOM/Animales/script.js
+++ Recuperacion/Ejercicios/EjercicicosDOM/Animales/script.js
@@ -1,4 +1,5 @@
 let animalSeleccionado = null;
+
 const distintasRazas = {
     gato: ["Siamens", "Persa", "Ragdoll"],
     perro: ["Labrador", "Bulldog", "Husky"]
@@ -6,51 +7,49 @@ const distintasRazas = {
 
 document.addEventListener("DOMContentLoaded", () => {
     mostrarDatos(animals); 
+    cargarFormulario(animals);
 });
 
+// Mostrar datos
 function mostrarDatos(data) {
     const tbody = document.querySelector("#animales-list tbody");
     tbody.innerHTML = ""; 
 
-    data.forEach((item, index) => {
+    data.forEach(item => {
         const fila = document.createElement("tr");
         fila.innerHTML = `
             <td>${item.name}</td>
             <td>${item.species}</td>
             <td>${item.foods.likes}</td>
             <td>${item.foods.dislikes}</td>
-            <td><button class="editar" data-index="${index}">Editar</button></td>
+            <td><button id="editar">Editar</button></td>
         `;
         tbody.appendChild(fila);
     });
-
-    /*tbody.addEventListener("click", (e) => {
-        if (e.target.classList.contains("editar")) {
-            const index = e.target.getAttribute("data-index");
-            cargarFormulario(animals[index]);
-        }
-        
-        if (e.target.closest('tr') && !e.target.classList.contains('editar')) {
-            const filaActual = e.target.closest('tr');
-            destacarFila(filaActual);
-        }
-    });*/
 }
 
-/*function cargarFormulario(animals) {
-    document.querySelector("#nombre").value = animals.name;
-    document.querySelector("#especie").value = animals.species;
-    document.querySelector("#likes").value = animals.foods.likes;
-    document.querySelector("#dislikes").value = animals.foods.dislikes;
-}*/
+function mostrarDatosJson(){
+    document.querySelector("#datos-json").textContent = JSON.stringify(animalSeleccionado, null, 2);
+}
 
-/*let filaAnteriorSeleccionada = null;
-
-function destacarFila(filaActual) {
-    if (filaAnteriorSeleccionada) {
-        filaAnteriorSeleccionada.classList.remove("fila-seleccionada");
-    }
+// Editar
+function editarDatos(){
+    const editar = document.querySelector("#editar");
     
-    filaActual.classList.add("fila-seleccionada");
-    filaAnteriorSeleccionada = filaActual;
-}*/
+    fila.classList.add ("fila-seleccionada");
+    
+}
+
+// Cargar formulario 
+function cargarFormulario (animal){
+    document.querySelector("#nombre").value = animal.name;
+
+    const selectRaza = document.querySelector("#raza");
+    selectRaza.innerHTML = ' <option value= "">--Raza--</option>'
+
+}
+
+// Validar
+function validarFormulario(){
+
+}
